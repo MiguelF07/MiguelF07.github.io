41e0228f45832b0683766b00f46b3f75764418a7
MiguelF07 <66647922+MiguelF07@users.noreply.github.com>
2020-06-08 23:21:34 +0100

    Marta

--- javascript/catalog.js
+++ javascript/catalog.js
@@ -1,123 +1,233 @@
-
-const productsCards = [{
-    image: "",
-    description: "Para-choques Volkswagen Gold 7 GTIc",
+var productsCards = [{
+    id: "prod-1",
+    image: "aapara chores 3.jpg",
+    name: "Para-choques Volkswagen Gold 7 GTIc",
     type: "Para-choques",
     sucata: "Servcarros",
     marca: "Volkswagen",
-    price:"700,00 €",
-    
+    price: "700,00 €",
+    priceInt: 700,
   }, {
-    image: "",
-    description: "Para-choques SEAT Léon Cupra R",
+    id: "prod-2",
+    image: "apara choques 2.jpg",
+    name: "Para-choques SEAT Léon Cupra R",
     type: "Para-choques",
     sucata: "Forjães",
     marca: "SEAT Léon",
-    price:"500,00 €",
+    price: "500,00 €",
+    priceInt: 500,
   }, {
-    image: "",
-    description: "Para-choques BMW M3 E46",
+    id: "prod-3",
+    image: "aaparachoques.jpg",
+    name: "Para-choques BMW M3 E46",
     type: "Para-choques",
     sucata: "SBL",
     marca: "BMW",
-    price:"400,00 €",
+    price: "400,00 €",
+    priceInt: 400,
   },
   {
-    image: "",
-    description: "Para-choques Mercedes A45",
+    id: "prod-4",
+    image: "aaparachoques 4.jpg",
+    name: "Para-choques Mercedes A45",
     type: "Para-choques",
     sucata: "SBL",
     marca: "Mercedes",
-    price:"800,00 €",
+    price: "800,00 €",
+    priceInt: 800,
   }, {
-    image: "",
-    description: "Jantes Mercedes C220",
+    id: "prod-5",
+    image: "aajantes4.jpg",
+    name: "Jantes Mercedes C220",
     type: "Jantes",
     sucata: "Servcarros",
     marca: "Mercedes",
-    price:"300,00 €",
+    price: "300,00 €",
+    priceInt: 300,
   }, {
-    image: "",
-    description: "Jantes GTI Golf 7",
+    id: "prod-6",
+    image: "aajantes3.png",
+    name: "Jantes GTI Golf 7",
     type: "Jantes",
     sucata: "SBL",
     marca: "Golf",
-    price:"350,00 €",
+    price: "350,00 €",
+    priceInt: 350,
   }, {
-    image: "",
-    description: "Jantes Audi A4",
+    id: "prod-7",
+    image: "aajantes2.jpg",
+    name: "Jantes Audi A4",
     type: "Jantes",
     sucata: "Forjães",
     marca: "Audi",
-    price:"150,00 €",
+    price: "150,00 €",
+    priceInt: 150,
   }, {
-    image: "Jantes BMW 18",
-    description: "desc1",
+    id: "prod-8",
+    image: "aajnate1.png",
+    name: "Jantes BMW 18",
     type: "Jantes",
     sucata: "SBL",
     marca: "BMW",
-    price:"250,00 €",
+    price: "250,00 €",
+    priceInt: 250,
   }, {
-    image: "",
-    description: "Motor Peugeot 508 1.6",
+    id: "prod-9",
+    image: "amotor5.jpg",
+    name: "Motor Peugeot 508 1.6",
     type: "Motor",
     sucata: "Servcarros",
     marca: "Peugeot",
-    price:"899,00 €",
+    price: "899,00 €",
+    priceInt: 899,
   }, {
-    image: "",
-    description: "Motor Volkswagen Amarok TDI 2.0",
+    id: "prod-10",
+    image: "amotor3.jpg",
+    name: "Motor Volkswagen Amarok TDI 2.0",
     type: "Motor",
     sucata: "SBL",
     marca: "Volkswagen",
-    price:"1499,00 €",
+    price: "1499,00 €",
+    priceInt: 1499,
   }, {
-    image: "",
-    description:"Motor Mercedes C220 CDI",
+    id: "prod-11",
+    image: "amotor2.jpg",
+    name: "Motor Mercedes C220 CDI",
     type: "Motor",
     sucata: "Forjães",
     marca: "Mercedes",
-    price:"999,00 €",
-  }, {
-    image: "",
-    description: "Motor BMW 116D",
+    price: "999,00 €",
+    priceInt: 999,
+  },
+  {
+    id: "prod-12",
+    image: "motor1.jpg",
+    name: "Motor BMW 116D",
     type: "Motor",
     sucata: "SBL",
     marca: "BMW",
-    price:"899,00 €",
-  },];
-  
-  
-  const container = document.getElementById('catalog');
-  
-  productsCards.forEach((result, idx) => {
+    price: "899,00 €",
+    priceInt: 899,
+  }
+];
+
+var container = document.getElementById('catalog');
+var modal = document.getElementById("modal-product");
+var localStorageKey = 'WheelDeal-Project-ShoppingCart';
+
+// Create the catalog on init of the Page
+createCards(productsCards);
+
+
+function createCards(arrayProds) {
+
+  container.innerHTML = '';
+
+  arrayProds.forEach((result, idx) => {
     // Create card element
-    const card = document.createElement('div');
+    var card = document.createElement('div');
     card.classList = 'card-body';
-  
+
     // Construct card content
-    const content = `
-      <div class="card">
-      <div class="card-header" id="heading-${idx}">
-        <h5 class="mb-0">
-          <button class="btn btn-link" data-toggle="collapse" data-target="#collapse-${idx}" aria-expanded="true" aria-controls="collapse-${idx}">
-  
-                  </button>
-        </h5>
-      </div>
-  
-      <div id="collapse-${idx}" class="collapse show" aria-labelledby="heading-${idx}" data-parent="#accordion">
-        <div class="card-body">
-  
-          <h5>${result.title}</h5>
-          <p>${result.description}</p>
-          <p>${result.output}</p>
-          ...
-        </div>
-      </div>
-    </div>
-    `;
-  
+    var content = `<div class="card column is-3" id="${result.id}" click>
+                      <div class="card-header" id="heading-${idx}">
+                        <img src="../imagens/${result.image}" width="100%"/>
+                      <span class="tag sucata">${result.sucata}</span>
+                      </div>
+                      <div id="collapse-${idx}" class="collapse show" aria-labelledby="heading-${idx}" data-parent="#accordion">
+                      <div class="card-body">
+                      
+                      <h5 class="title is-5">${result.name}</h5>
+                      <hr class="hr-minor">
+                      <p>${result.price}</p>
+                      </div>
+                        <p class="object-result" style="display:none">${JSON.stringify(result)}</p>
+                        </div>
+                    </div>
+                      `;
+
     // Append newyly created card element to the container
     container.innerHTML += content;
-  })
+  });
+
+}
+
+
+// Add Event Listener to Click on Card
+var el = document.querySelectorAll('.card');
+for (var i = 0; i < el.length; i++) {
+  el[i].addEventListener('click', function (event) {
+    var iterationProductId = event.currentTarget.id;
+    var valueOfProd = productsCards.filter(x => x.id === iterationProductId);
+    createModalContent(valueOfProd[0]);
+    openProductModal();
+  });
+}
+
+// Create the modal based on product selected
+function createModalContent(value) {
+  // Clean the modal 
+  var productModal = document.getElementById("modal-content-product");
+
+  var productName = productModal.querySelector('.product-name');
+  var productPrice = productModal.querySelector('.product-price');
+  var productImage = productModal.querySelector('.product-img');
+  var productId = productModal.querySelector('.modal-product-id');
+
+  // Set the values with current product
+  productName.innerHTML = value.name;
+  productPrice.innerHTML = value.price;
+  productImage.src = '../imagens/' + value.image;
+  productId.innerHTML = value.id;
+
+}
+
+// MODAL FUNCTIONS
+function openProductModal() {
+  modal.classList.toggle('is-active');
+}
+
+function closeModal() {
+  modal.classList.remove('is-active');
+}
+
+
+// Add the product to ShoppingCart
+function addToShoppingCart() {
+
+  var productId = document.querySelector(".modal-product-id").innerHTML;
+
+  var valuesOnLS = localStorage.getItem(localStorageKey);
+  var valuesOnLSJSON = valuesOnLS == null ? [] : JSON.parse(valuesOnLS);
+
+  var indexOnLS = valuesOnLSJSON.findIndex(v => v.prod.id === productId);
+
+  if (indexOnLS === -1) {
+    product = {
+      prod: productsCards.find(x => x.id === productId),
+      qty: 1
+    };
+    valuesOnLSJSON.push(product);
+  } else {
+    valuesOnLSJSON[indexOnLS].qty += 1;
+  }
+
+  localStorage.setItem(localStorageKey, JSON.stringify(valuesOnLSJSON));
+  closeModal();
+}
+
+// w3schools accordion example
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
--- javascript/shoppingcart.js
+++ javascript/shoppingcart.js
@@ -1,33 +1,201 @@
+// $('.minus-btn').on('click', function (e) {
+//     e.preventDefault();
+//     var $this = $(this);
+//     var $input = $this.closest('div').find('input');
+//     var value = parseInt($input.val());
 
-$('.minus-btn').on('click', function(e) {
-    e.preventDefault();
-    var $this = $(this);
-    var $input = $this.closest('div').find('input');
-    var value = parseInt($input.val());
- 
-    if (value > 1) {
-        value = value - 1;
+//     if (value > 1) {
+//         value = value - 1;
+//     } else {
+//         value = 0;
+//     }
+
+//     $input.val(value);
+
+// });
+
+// $('.plus-btn').on('click', function (e) {
+//     e.preventDefault();
+//     var $this = $(this);
+//     var $input = $this.closest('div').find('input');
+//     var value = parseInt($input.val());
+
+//     if (value < 100) {
+//         value = value + 1;
+//     } else {
+//         value = 100;
+//     }
+
+//     $input.val(value);
+// });
+
+
+// Get Value Of LocalStorage
+var localStorageKey = 'WheelDeal-Project-ShoppingCart';
+var valuesOnLS = localStorage.getItem(localStorageKey);
+var valuesOnLSJSON = JSON.parse(valuesOnLS);
+var container = document.getElementById('shopping-cart-items');
+var priceTotal = document.getElementById('total-price');
+
+// Create the catalog on init of the Page
+if (valuesOnLSJSON && valuesOnLSJSON.length > 0) {
+    createItems(valuesOnLSJSON);
+} else {
+    createNoProdsMessage();
+}
+
+function createNoProdsMessage() {
+    container.innerHTML = '<p>Não tens produtos adicionados ao Shopping Cart!</p>';
+}
+
+function createItems(arrayProds) {
+
+    container.innerHTML = '';
+    var _priceTotal = 0;
+    arrayProds.forEach((result, idx) => {
+
+        _priceTotal += result.qty * result.prod.priceInt;
+
+        // Create card element
+        var card = document.createElement('div');
+        card.classList = 'card-body';
+
+        // Construct card content
+        var content = `<div class="item" id="${result.prod.id}">
+        <button class="button remove-button" id="${result.prod.id}">Remove</button>
+        
+        <div class="image" style="width:85px">
+            <img src="../imagens/${result.prod.image}" alt="" width="100%" />
+        </div>
+
+        <div class="quantity" >
+            <button class="minus-btn${result.qty === 1 ? ' block-button' : ''}" id="${result.prod.id}" type="button" name="button">
+                <img src="../imagens/minus.svg" alt="" />
+            </button>
+            <input id="${result.prod.id}" type="text" name="name" value="${result.qty}">
+            <button class="plus-btn" id="${result.prod.id}" type="button" name="button">
+                <img src="../imagens/plus.svg" alt="" /> 
+            </button>
+        </div>
+        
+        <div class="description">
+            <span>${result.prod.name}</span>
+            <span>${result.prod.sucata}</span>
+        </div>
+                
+        <div class="total-price">${result.prod.priceInt * result.qty}</div>
+      </div>`;
+
+        // Append newyly created card element to the container
+        container.innerHTML += content;
+        priceTotal.innerHTML = _priceTotal;
+    });
+}
+
+function calculateTotalPrice(prods) {
+
+    if(prods == null || prods.length === 0) {
+        priceTotal.innerHTML = 0;
+
     } else {
-        value = 0;
+        var _priceTotal = 0;
+        prods.forEach((result, idx) => {
+            _priceTotal += result.qty * result.prod.priceInt;
+            priceTotal.innerHTML = _priceTotal;
+        });
     }
- 
-  $input.val(value);
- 
-});
- 
-$('.plus-btn').on('click', function(e) {
-    e.preventDefault();
-    var $this = $(this);
-    var $input = $this.closest('div').find('input');
-    var value = parseInt($input.val());
- 
-    if (value < 100) {
-        value = value + 1;
+}
+
+// Add Event Listener to plus button
+var elPlus = document.querySelectorAll('#shopping-cart-items .item .quantity .plus-btn');
+for (var i = 0; i < elPlus.length; i++) {
+    elPlus[i].addEventListener('click', function (event) {
+        var iterationProductId = event.currentTarget.id;
+        addRemoveQty(true, iterationProductId, event.currentTarget);
+    });
+}
+
+// Add Event Listener to minus button
+var elMinus = document.querySelectorAll('#shopping-cart-items .item .quantity .minus-btn');
+for (var i = 0; i < elMinus.length; i++) {
+    elMinus[i].addEventListener('click', function (event) {
+        var iterationProductId = event.currentTarget.id;
+        addRemoveQty(false, iterationProductId, event.currentTarget);
+    });
+}
+
+// Add Event Listener to remove button
+var elRemove = document.querySelectorAll('#shopping-cart-items .item .remove-button');
+for (var i = 0; i < elRemove.length; i++) {
+    elRemove[i].addEventListener('click', function (event) {
+        var iterationProductId = event.currentTarget.id;
+        removeFromCart(iterationProductId);
+    });
+}
+
+
+function addRemoveQty(add, iterationProductId, element) {
+    var valuesOnLS = localStorage.getItem(localStorageKey);
+    var valuesOnLSJSON = JSON.parse(valuesOnLS);
+
+    var idLS = valuesOnLSJSON.findIndex(x => x.prod.id === iterationProductId);
+
+    if (add) {
+        valuesOnLSJSON[idLS].qty += 1;
     } else {
-        value =100;
+        if (valuesOnLSJSON[idLS].qty > 1) {
+            valuesOnLSJSON[idLS].qty -= 1;        
+        }
     }
- 
-    $input.val(value);
-});
 
 
+    var buttonMinus = document.querySelector('#shopping-cart-items .item .quantity .minus-btn#' + iterationProductId);
+    if (valuesOnLSJSON[idLS].qty === 1) {
+        // Disable the button
+        buttonMinus.classList.add('block-button');
+    } else {
+        // enable the button
+        buttonMinus.classList.remove('block-button');
+    }
+
+    calculateTotalPrice(valuesOnLSJSON);
+
+    var search = '#shopping-cart-items .item .quantity input#' + iterationProductId;
+    document.querySelector(search).value = valuesOnLSJSON[idLS].qty;
+    
+    localStorage.setItem(localStorageKey, JSON.stringify(valuesOnLSJSON));
+}
+
+function removeFromCart(iterationProductId){
+    var valuesOnLS = localStorage.getItem(localStorageKey);
+    var valuesOnLSJSON = JSON.parse(valuesOnLS);
+    var newValues = valuesOnLSJSON.filter(x => x.prod.id !== iterationProductId);
+
+    localStorage.setItem(localStorageKey, JSON.stringify(newValues));
+
+    calculateTotalPrice(newValues);
+
+    if(valuesOnLSJSON.length === 0) {
+        createNoProdsMessage();
+    }
+    var elToRemove = document.querySelector('#shopping-cart-items .item#' + iterationProductId);
+    elToRemove.style.display = "none";
+}
+
+var modal = document.getElementById("modal-product");
+
+function openProductModal() {
+    modal.classList.toggle('is-active');
+}
+
+function closeModal() {
+    modal.classList.remove('is-active');
+}
+
+function showInput() {
+    document.getElementById("cuppon-area").classList.toggle('is-active');
+}
+
+function showNote() {
+    document.getElementById("note-area").classList.toggle('is-active');
+}
